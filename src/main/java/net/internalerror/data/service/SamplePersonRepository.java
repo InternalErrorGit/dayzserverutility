8af43cd01f58c388f7bae50775de2a052e237dd4
start.vaadin.com <no-reply@vaadin.com>
2023-04-18 06:27:34 +0000

    Generated project

---
+++ src/main/java/net/internalerror/data/service/SamplePersonRepository.java
@@ -0,0 +1,12 @@
+package net.internalerror.data.service;
+
+import net.internalerror.data.entity.SamplePerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SamplePersonRepository
+        extends
+            JpaRepository<SamplePerson, Long>,
+            JpaSpecificationExecutor<SamplePerson> {
+
+}
